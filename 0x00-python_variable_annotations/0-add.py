dbe5179cbab0a1579bbb30400d84c7be3aad37ca
Ambesawi <ambesawi77@gmail.com>
2023-10-06 15:43:18 -0700

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/0-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+add = __import__('0-add').add
+
+print(add(1.11, 2.22) == 1.11 + 2.22)
+print(add.__annotations__)
